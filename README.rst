bc3694b84258b2b4f29ac9dd5ffbecca44ac2027
Jonas Baumann <jone@jone.ch>
2011-07-04 13:54:01 +0200

    created package with paster

---
+++ README.rst
@@ -0,0 +1,26 @@
+Introduction
+============
+
+
+This is a plugin for `TinyMCE`__ editor for Plone.
+
+__ http://plone.org/products/tinymce/
+
+It provides additional toolbar icons for providing a tooltip
+on the selected text.
+
+The tooltip is displayed with `jQuery Tools`__
+
+__ http://flowplayer.org/tools/index.html
+
+
+Credits
+=======
+
+Sponsered by `4teamwork`_.
+
+ * `Jonas Baumann`_, author
+
+
+.. _`4teamwork`: http://www.4teamwork.ch/
+.. _`Jonas Baumann`: http://github.com/jone
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0-dev'
+
+setup(name='collective.tinymceplugins.tooltip',
+      version=version,
+      description="TinyMCE plugin for creating tooltips on text",
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        ],
+
+      keywords='tinymce plugin tooltip',
+      author='Jonas Baumann, 4teamwork.ch',
+      author_email='mailto:info@4teamwork.ch',
+
+      url='http://github.com/4teamwork/collective.tinymceplugins.tooltip',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.tinymceplugins'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        'Products.TinyMCE',
+        ],
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
