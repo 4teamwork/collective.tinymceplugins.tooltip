9635064f3ab6f62d3aca4f3a3380a19db5c1a570
Jonas Baumann <jone@jone.ch>
2011-07-04 14:23:17 +0200

    fixed path to readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0-dev'
 setup(name='collective.tinymceplugins.tooltip',
       version=version,
       description="TinyMCE plugin for creating tooltips on text",
-      long_description=open("README.txt").read() + "\n" + \
+      long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
 
       classifiers=[
