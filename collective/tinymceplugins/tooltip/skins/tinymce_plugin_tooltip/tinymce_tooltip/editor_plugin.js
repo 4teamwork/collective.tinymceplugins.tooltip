7114e4b1070339417f9f0b3756d5ef04ae3e3c78
Jonas Baumann <jone@jone.ch>
2011-07-04 16:23:29 +0200

    implemented tooltip plugin

--- collective/tinymceplugins/tooltip/skins/tinymce_plugin_tooltip/tinymce_tooltip/editor_plugin.js
+++ collective/tinymceplugins/tooltip/skins/tinymce_plugin_tooltip/tinymce_tooltip/editor_plugin.js
@@ -3,55 +3,68 @@
  */
 
 (function() {
-    tinymce.create('tinymce.plugins.Tooltip', {
-        init : function(ed, url) {
-            this.editor = ed;
+  tinymce.create('tinymce.plugins.Tooltip', {
+    init : function(ed, url) {
+      this.editor = ed;
+      console.log('LOADING TOOLTIP');
 
-            // Register commands
-            ed.addCommand('mceTooltip', function() {
-                var se = ed.selection;
+      // Register commands
+      ed.addCommand('mceTooltip', function() {
+        var se = ed.selection;
 
-                // No selection and not in link
-                if (se.isCollapsed() && !ed.dom.getParent(se.getNode(), 'A'))
-                    return;
-
-                ed.windowManager.open({
-                    file : url + '/tooltip.htm',
-                    width : 820 + parseInt(
-                      ed.getLang('plonelink.delta_width', 0), 10),
-                    height : 500 + parseInt(
-                      ed.getLang('plonelink.delta_height', 0), 10),
-                    inline : 1
-                }, {
-                    plugin_url : url
-                });
-            });
-
-            // Register buttons
-            ed.addButton('link', {
-                title : 'advanced.tooltip_desc',
-                cmd : 'mceTooltip'
-            });
-
-            ed.addShortcut('', 'advanced.tooltip_desc', 'mceTooltip');
-
-            // ed.onNodeChange.add(function(ed, cm, n, co) {
-            //     cm.setDisabled('link', co && n.nodeName != 'A');
-            //     cm.setActive('link', n.nodeName == 'A' && !n.name);
-            // });
-        },
-
-        getInfo : function() {
-            return {
-                longname : 'tooltip plugin',
-                author : 'jone',
-                authorurl : 'http://www.4teamwokr.ch/',
-                infourl : 'http://github.com/4teamwork/collective.tinymceplugins.tooltip',
-                version : tinymce.majorVersion + "." + tinymce.minorVersion
-            };
+        var span = ed.dom.getParent(ed.selection.getNode(), 'span');
+        var within_tooltip = (span && span.getAttribute(
+          'class') && span.getAttribute('class').indexOf('tooltip') > -1);
+        if (se.isCollapsed() && !within_tooltip) {
+          return;
         }
-    });
 
-    // Register plugin
-    tinymce.PluginManager.add('tooltip', tinymce.plugins.PloneLinkPlugin);
+        ed.windowManager.open({
+          file : url + '/tooltip.htm',
+          width : 320 + parseInt(
+            ed.getLang('advanced.anchor_delta_width', 0), 10),
+          height : 90 + parseInt(
+            ed.getLang('advanced.anchor_delta_height', 0), 10),
+          inline : 1
+        }, {
+          plugin_url : url
+        });
+      });
+
+      // Register buttons
+      ed.addButton('tinymce_tooltip', {
+        title : 'advanced.tooltip_desc',
+        cmd : 'mceTooltip',
+        image: url + '/img/tooltip.gif'
+      });
+
+      ed.onNodeChange.add(function(ed, cm, n, co) {
+        cm.setDisabled('tinymce_tooltip', co && n.nodeName != 'span');
+        cm.setActive('tinymce_tooltip', n.nodeName == 'span' && !n.title);
+
+        var selecting = !co;
+        var span = ed.dom.getParent(ed.selection.getNode(), 'span');
+        if (span) {
+          var enabled = (span.getAttribute('class') &&
+                          span.getAttribute('class').indexOf('tooltip') > -1);
+
+          cm.setActive('tinymce_tooltip', enabled);
+          cm.setDisabled('tinymce_tooltip', !enabled && !selecting);
+        }
+      });
+    },
+
+    getInfo : function() {
+      return {
+        longname : 'tooltip plugin',
+        author : 'jone',
+        authorurl : 'http://www.4teamwork.ch/',
+        infourl : 'http://github.com/4teamwork/collective.tinymceplugins.tooltip',
+        version : tinymce.majorVersion + "." + tinymce.minorVersion
+      };
+    }
+  });
+
+  // Register plugin
+  tinymce.PluginManager.add('tinymce_tooltip', tinymce.plugins.Tooltip);
 })();
---
+++ collective/tinymceplugins/tooltip/skins/tinymce_plugin_tooltip/tinymce_tooltip/js/tooltip.js
@@ -0,0 +1,64 @@
+tinyMCEPopup.requireLangPack();
+
+var TooltipDialog = {
+  init : function(ed) {
+    var action, elm, f = document.forms[0];
+
+    this.editor = ed;
+    elm = ed.dom.getParent(ed.selection.getNode(), 'span');
+    v = ed.dom.getAttrib(elm, 'title');
+
+    if (v) {
+      this.action = 'update';
+      f.tooltipName.value = v;
+    }
+
+    f.insert.value = ed.getLang(elm ? 'update' : 'insert');
+  },
+
+  update : function() {
+    var ed = this.editor, elm, tooltip = document.forms[0].tooltipName.value;
+
+    tinyMCEPopup.restoreSelection();
+
+    elm = ed.dom.getParent(ed.selection.getNode(), 'span');
+
+    /* if no tooltip text is defined and there was / is a
+       tooltip span, remove the span */
+    if (!tooltip) {
+      if (elm) {
+        b = ed.selection.getBookmark();
+        ed.dom.remove(elm, 1);
+        ed.selection.moveToBookmark(b);
+        tinyMCEPopup.execCommand("mceEndUndoLevel");
+        tinyMCEPopup.close();
+        return;
+      }
+    }
+
+    if (elm) {
+      /* update the tooltip title */
+      elm.title = tooltip;
+
+      /* add the tooltip class if missing. this happens when there
+         already was a span tag */
+      if (!elm.getAttribute('class')) {
+        elm.setAttribute('class', 'tooltip');
+      } else if (elm.getAttribute('tooltip') == -1) {
+        elm.setAttribute('class', elm.getAttribute('class') + ' tooltip');
+      }
+
+    } else {
+      /* create a new span tag around the selected text */
+      ed.execCommand('mceInsertContent', 0,
+                     ed.dom.createHTML(
+                       'span', {title: tooltip,
+                                'class' : 'tooltip'},
+                       ed.selection.getContent({format : 'text'})));
+    }
+
+    tinyMCEPopup.close();
+  }
+};
+
+tinyMCEPopup.onInit.add(TooltipDialog.init, TooltipDialog);
