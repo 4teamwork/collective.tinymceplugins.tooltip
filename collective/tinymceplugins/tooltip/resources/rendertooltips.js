10866c8d94a9898d270788ff9af92201e60cc541
Jonas Baumann <jone@jone.ch>
2011-07-11 15:12:00 +0200

    css-only implementation

--- collective/tinymceplugins/tooltip/resources/rendertooltips.js
+++ collective/tinymceplugins/tooltip/resources/rendertooltips.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function($) {
-  $('span.tooltip[title]').tooltip();
+  $('span.tooltip[title]').tooltip({
+
+    layout: '<div><div class="tooltip-arrow-border"></div>' +
+      '<div class="tooltip-arrow"></div></div>',
+
+    delay: 200,
+
+    effect: 'fade',
+
+    onBeforeShow: function(ob, pos) {
+      var arrow_left = this.getTrigger().position().left - pos.left + 22;
+      this.getTip().find('div.tooltip-arrow').css('left', arrow_left);
+      this.getTip().find('div.tooltip-arrow-border').css('left', arrow_left);
+    }
+
+  }).dynamic();
 });
